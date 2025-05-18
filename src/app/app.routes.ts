4abc4bbe8cd276b75630d676fb2b5d56bebb8f57
Rula-Sh <1.rulashaban@gmail.com>
2025-05-18 18:52:55 +0300

    Add Routes, authGaurd, and AuthService

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,182 @@
-import { Routes } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  Router,
+  RouterStateSnapshot,
+  Routes,
+} from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { inject } from '@angular/core';
+import { HomeRedirectComponent } from './components/shared/home-redirect/home-redirect.component';
+import { LoginComponent } from './components/shared/login/login.component';
+import { SignUpComponent } from './components/shared/sign-up/sign-up.component';
+import { ProfileComponent } from './components/shared/profile/profile.component';
+import { NotAuthorizedComponent } from './components/shared/not-authorized/not-authorized.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
+import { HomeComponent } from './components/customer/home/home.component';
+import { ReservationDetailsComponent } from './components/customer/reservation-details/reservation-details.component';
+import { MyReservationsComponent } from './components/customer/my-reservations/my-reservations.component';
+import { AvailableServicesComponent } from './components/customer/available-services/available-services.component';
+import { ServicesRequestsComponent } from './components/customer/services-requests/services-requests.component';
+import { RequestsComponent } from './components/employee/requests/requests.component';
+import { RequestDetailsComponent } from './components/employee/request-details/request-details.component';
+import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
+import { AddEmployeeComponent } from './components/admin/add-employee/add-employee.component';
+import { ManageUsersComponent } from './components/admin/manage-users/manage-users.component';
+import { UserDetailsComponent } from './components/admin/user-details/user-details.component';
+import { AddRoomOrHallComponent } from './components/admin/add-room-or-hall/add-room-or-hall.component';
+import { ManageRoomsAndHallsComponent } from './components/admin/manage-rooms-and-halls/manage-rooms-and-halls.component';
+import { ReservationsComponent } from './components/admin/reservations/reservations.component';
+import { ServicesComponent } from './components/employee/services/services.component';
+import { AddServiceComponent } from './components/employee/add-service/add-service.component';
+import { ServiceDetailsComponent } from './components/employee/service-details/service-details.component';
 
-export const routes: Routes = [];
+export const authGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot
+) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+  const token = localStorage.getItem('auth_token');
+
+  // If the user is not logged in, redirect to login page
+  if (!token) {
+    authService.setRedirectUrl(state.url); // Save the attempted URL for redirecting
+    return router.createUrlTree(['/login']);
+  }
+
+  // redirect to /not-autherized if the user tries to access a page that is not specified to their role
+  const expectedRoles = route.data['roles'] as string[] | undefined;
+
+  const userRole = authService.getUserRole() ?? '';
+
+  if (expectedRoles && !expectedRoles.includes(userRole)) {
+    return router.createUrlTree(['/not-autherized']);
+  }
+
+  return true;
+};
+
+export const routes: Routes = [
+  // -------- All Users --------
+
+  { path: '', component: HomeRedirectComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
+  {
+    path: 'profile/:id',
+    component: ProfileComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Admin', 'Employee', 'Customer'] },
+  },
+
+  { path: 'not-autherized', component: NotAuthorizedComponent },
+  { path: '**', component: NotFoundComponent },
+
+  // ------------------------ Customer ------------------------
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'reservation-details/:id',
+    component: ReservationDetailsComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Customer', 'Admin'] },
+  },
+  {
+    path: 'my-reservations',
+    component: MyReservationsComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Customer'] },
+  },
+  {
+    path: 'available-services',
+    component: AvailableServicesComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Customer'] },
+  },
+  {
+    path: 'service-requests',
+    component: ServicesRequestsComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Customer'] },
+  },
+
+  // ------------------------ Emplyoyee ------------------------
+  {
+    path: 'services',
+    component: ServicesComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Employee'] },
+  },
+  {
+    path: 'add-service',
+    component: AddServiceComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Employee'] },
+  },
+  {
+    path: 'service-details/:id',
+    component: ServiceDetailsComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Employee'] },
+  },
+  {
+    path: 'requests',
+    component: RequestsComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Employee'] },
+  },
+  {
+    path: 'request-details/:id',
+    component: RequestDetailsComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Employee'] },
+  },
+
+  // ------------------------ Admin ------------------------
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Admin'] },
+  },
+  {
+    path: 'add-employee',
+    component: AddEmployeeComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Admin'] },
+  },
+  {
+    path: 'manage-users',
+    component: ManageUsersComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Admin'] },
+  },
+  {
+    path: 'user-details/:id',
+    component: UserDetailsComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Admin'] },
+  },
+  {
+    path: 'add-room-or-hall',
+    component: AddRoomOrHallComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Admin'] },
+  },
+  {
+    path: 'manage-rooms-and-halls',
+    component: ManageRoomsAndHallsComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Admin'] },
+  },
+  {
+    path: 'reservations',
+    component: ReservationsComponent,
+    canActivate: [authGuard],
+    data: { roles: ['Admin'] },
+  },
+  // reservation-details/:id route is shared with the customer
+];
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/User';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private loggedIn = false;
+  private redirectUrl = '';
+  constructor() {
+    const savedUser = localStorage.getItem('user');
+    if (savedUser) {
+      this.loggedIn = true;
+    }
+  }
+
+  login(user: User) {
+    this.loggedIn = true;
+    const mockToken = 'mock-token';
+    localStorage.setItem('auth_token', mockToken);
+    localStorage.setItem('id', user.id);
+    localStorage.setItem('username', user.username);
+    localStorage.setItem('email', user.email);
+    localStorage.setItem('user_role', user.role);
+  }
+
+  logout() {
+    this.loggedIn = false;
+    localStorage.removeItem('auth_token');
+    localStorage.removeItem('id');
+    localStorage.removeItem('username');
+    localStorage.removeItem('email');
+    localStorage.removeItem('user_role');
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('auth_token');
+  }
+
+  getCurrentUser(): User | null {
+    const id = localStorage.getItem('id');
+    const username = localStorage.getItem('username');
+    const email = localStorage.getItem('email');
+    const role = localStorage.getItem('user_role');
+    if (username && email && role) {
+      const user: User = { id, username, email, role } as User;
+      return user;
+    }
+    return null;
+  }
+
+  setRedirectUrl(url: string) {
+    this.redirectUrl = url;
+  }
+
+  getRedirectURL(): string | null {
+    return this.redirectUrl;
+  }
+
+  getUserRole(): string | null {
+    return localStorage.getItem('user_role');
+  }
+}
