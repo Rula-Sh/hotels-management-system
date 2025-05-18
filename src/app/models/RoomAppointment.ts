b189e1b9870322e84cd0111850f23166e1fb2267
Rula-Sh <1.rulashaban@gmail.com>
2025-05-18 19:14:29 +0300

    Add ID

--- src/app/models/RoomAppointment.ts
+++ src/app/models/RoomAppointment.ts
@@ -1,6 +1,7 @@
 import { Room } from './Room';
 
 export interface RoomAppointment extends Room {
+  id: string;
   date: Date;
   customerId: string;
   appovalStatus: 'Pending' | 'Approved' | 'Rejected';
