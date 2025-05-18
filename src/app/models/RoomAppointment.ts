66b339377ad448719130b21d254de2e74e57955e
Rula-Sh <1.rulashaban@gmail.com>
2025-05-18 21:20:30 +0300

    Remove id

--- src/app/models/RoomAppointment.ts
+++ src/app/models/RoomAppointment.ts
@@ -1,7 +1,6 @@
 import { Room } from './Room';
 
 export interface RoomAppointment extends Room {
-  id: string;
   date: Date;
   customerId: string;
   appovalStatus: 'Pending' | 'Approved' | 'Rejected';
