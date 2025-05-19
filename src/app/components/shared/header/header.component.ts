2dd458682e651e5a8af566cad2f8af6e10164c80
Rula-Sh <1.rulashaban@gmail.com>
2025-05-19 20:24:39 +0300

    Add Header Content

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { I18nPipe } from '../../../pipes/i18n.pipe';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [I18nPipe, RouterModule, CommonModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  userId: string | null = null;
+  name: string | null = null;
+  role: string | null = null;
 
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userId = localStorage.getItem('id');
+    this.name = localStorage.getItem('name');
+    this.role = localStorage.getItem('user_role');
+  }
+
+  ngDoCheck() {
+    this.userId = localStorage.getItem('id');
+    this.name = localStorage.getItem('username');
+    this.role = localStorage.getItem('user_role');
+  }
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate([`/`]);
+  }
+
+  goToProfile(id: string) {
+    this.router.navigate([`profile/${id}`]);
+  }
 }
