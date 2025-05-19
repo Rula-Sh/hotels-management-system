eb5ed401d0adbde04ecfa7b0fa7d0c569b260635
Rula-Sh <1.rulashaban@gmail.com>
2025-05-19 23:34:48 +0300

    Add Redirect Logic Based on Role

--- src/app/components/shared/auth/home-redirect/home-redirect.component.ts
+++ src/app/components/shared/auth/home-redirect/home-redirect.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-redirect',
   imports: [],
   templateUrl: './home-redirect.component.html',
-  styleUrl: './home-redirect.component.scss'
+  styleUrl: './home-redirect.component.scss',
 })
 export class HomeRedirectComponent {
+  role: string | null = null;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.role = localStorage.getItem('user_role');
+    if (this.role == 'Admin') {
+      this.router.navigate([`/reservations`]);
+    } else if (this.role == 'Employee') {
+      this.router.navigate([`/requests`]);
+    } else {
+      this.router.navigate([`/home`]);
+    }
+  }
 }
