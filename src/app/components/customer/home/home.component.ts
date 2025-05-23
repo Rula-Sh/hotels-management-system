d363f742bf88e20500c1b014ff209d1f70d7f4db
Areen-118 <test@DESKTOP-KB7VL1H>
2025-05-23 20:25:51 +0300

    home page

--- src/app/components/customer/home/home.component.ts
+++ src/app/components/customer/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { SignUpComponent } from "../../shared/auth/sign-up/sign-up.component";
 import { LoginComponent } from '../../shared/auth/login/login.component';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [LoginComponent, SignUpComponent, RouterLink],
+  imports: [ RouterLink,RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
