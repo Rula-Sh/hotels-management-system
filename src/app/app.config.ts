e8f40cbeaa0efd2eb988907c908436c8dca5aca1
Areen-118 <test@DESKTOP-KB7VL1H>
2025-05-23 20:28:47 +0300

    backend ,css,toast

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { provideToastr } from 'ngx-toastr';
+
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -11,6 +13,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideToastr( 
+       {positionClass: 'toast-top-center', // وسط أعلى الشاشة
+      timeOut: 3000,
+      closeButton: true,
+      progressBar: true,
+      preventDuplicates: true,}),
     provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
