ff15a5c3f6f2dc26203d91df363999ae28ff3273
Rula-Sh <1.rulashaban@gmail.com>
2025-05-25 19:21:01 +0300

    Add Employee To Service

--- src/app/models/Service.model.ts
+++ src/app/models/Service.model.ts
@@ -1,6 +1,10 @@
+import { Employee } from './Employee.model';
+
 export interface Service {
   id: string;
+  employeeId: string;
   title: string;
   details: string;
   price: number;
+  employee: Employee;
 }
