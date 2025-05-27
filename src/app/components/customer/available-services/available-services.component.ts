6a730466402be6d62e82d858b7727db04bd14f97
Areen-118 <test@DESKTOP-KB7VL1H>
2025-05-27 19:40:52 +0300

    add avaliable services page

--- src/app/components/customer/available-services/available-services.component.ts
+++ src/app/components/customer/available-services/available-services.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit,inject} from '@angular/core';
+import { Service } from '../../../models/Service.model';
+import { ServiceService } from '../../../services/service.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-available-services',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './available-services.component.html',
   styleUrl: './available-services.component.scss'
 })
-export class AvailableServicesComponent {
+export class AvailableServicesComponent implements OnInit {
+  services: Service[] = [];
+  private serviceService = inject(ServiceService);
+
+  ngOnInit(): void {
+    this.loadAvailableServices();
+  }
+
+  loadAvailableServices(): void {
+    this.serviceService.getAllServices().subscribe({
+      next: (services: Service[]) => {
+        this.services = services;
+      },
+      error: (error) => {
+        console.error('Error loading available services:', error);
+      }
+    });
+  }
 
 }
