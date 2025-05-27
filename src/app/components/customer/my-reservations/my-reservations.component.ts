21eca1cd85bf92aab6a1a42b204f8df1cb058bd3
Areen-118 <test@DESKTOP-KB7VL1H>
2025-05-27 19:44:41 +0300

    add my reservations page

--- src/app/components/customer/my-reservations/my-reservations.component.ts
+++ src/app/components/customer/my-reservations/my-reservations.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../../../services/reservation.service';
+import { Reservation } from '../../../models/Reservation.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-my-reservations',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-reservations.component.html',
-  styleUrl: './my-reservations.component.scss'
+  styleUrls: ['./my-reservations.component.scss'],
 })
-export class MyReservationsComponent {
+export class MyReservationsComponent implements OnInit {
+  constructor(private reservationService: ReservationService) {}
+
+  reservations: Reservation[] = [];
+
+  ngOnInit(): void {
+    const userId = localStorage.getItem('user_id');
+    if (userId) {
+      this.reservationService.getReservationsByCustomerId(userId).subscribe({
+        next: (res) => (this.reservations = res),
+      });
+  }
+}
+  cancelReservation(id: string) {
+  this.reservationService.cancelReservation(id).subscribe({
+    next: () => {
+      this.reservations = this.reservations.filter(r => r.id !== id);
+    },
+    error: (err) => {
+      console.error('Failed to cancel reservation', err);
+    }
+  });
+}
 
 }
