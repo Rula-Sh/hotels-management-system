09b711f8d105f007d1fb87d0e7ff7db56edcfb93
Rula-Sh <1.rulashaban@gmail.com>
2025-05-28 20:25:49 +0300

    Populate Employee Requests Page

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Service } from '../models/Service.model';
+import { ServiceRequest } from '../models/ServiceRequest.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,25 +10,55 @@ import { Service } from '../models/Service.model';
 export class ServiceService {
   constructor() {}
   api = inject(HttpClient);
-  url = 'http://localhost:3000/services';
+  servicesUrl = 'http://localhost:3000/services';
+  servicesRequestsUrl = 'http://localhost:3000/serviceRequests';
 
   getAllServices(): Observable<Service[]> {
-    return this.api.get<Service[]>(this.url);
+    return this.api.get<Service[]>(this.servicesUrl);
   }
 
   getServiceById(id: string): Observable<Service> {
-    return this.api.get<Service>(`${this.url}/${id}`);
+    return this.api.get<Service>(`${this.servicesUrl}/${id}`);
   }
 
   getServicesByEmployeeId(employeeId: string): Observable<Service[]> {
-    return this.api.get<Service[]>(`${this.url}/?employeeId=${employeeId}`);
+    return this.api.get<Service[]>(
+      `${this.servicesUrl}/?employeeId=${employeeId}`
+    );
   }
-
   CreateService(service: Omit<Service, 'id'>): Observable<Service> {
-    return this.api.post<Service>(this.url, service);
+    return this.api.post<Service>(this.servicesUrl, service);
   }
 
   UpdateService(service: Service): Observable<Service> {
-    return this.api.put<Service>(`${this.url}/${service.id}`, service);
+    return this.api.put<Service>(`${this.servicesUrl}/${service.id}`, service);
+  }
+
+  getServicesRequestsByEmployeeId(
+    employeeId: string
+  ): Observable<ServiceRequest[]> {
+    return this.api.get<ServiceRequest[]>(
+      `${this.servicesRequestsUrl}/?employeeId=${employeeId}`
+    );
+  }
+
+  ApproveServicesRequest(
+    id: string,
+    request: ServiceRequest
+  ): Observable<ServiceRequest> {
+    return this.api.put<ServiceRequest>(
+      `${this.servicesRequestsUrl}/${id}`,
+      request
+    );
+  }
+
+  RejectServicesRequest(
+    id: string,
+    request: ServiceRequest
+  ): Observable<ServiceRequest> {
+    return this.api.put<ServiceRequest>(
+      `${this.servicesRequestsUrl}/${id}`,
+      request
+    );
   }
 }
