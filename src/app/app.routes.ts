b1ae93bbb831dfd47b7b7db9efaef5f3094bfc4a
Rula-Sh <1.rulashaban@gmail.com>
2025-05-29 13:59:03 +0300

    Remove RequestDetails and ServiceDetails

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,7 +16,6 @@ import { NotFoundComponent } from './components/shared/not-found/not-found.compo
 import { MyReservationsComponent } from './components/customer/my-reservations/my-reservations.component';
 import { AvailableServicesComponent } from './components/customer/available-services/available-services.component';
 import { RequestsComponent } from './components/employee/requests/requests.component';
-import { RequestDetailsComponent } from './components/employee/request-details/request-details.component';
 import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 import { AddEmployeeComponent } from './components/admin/add-employee/add-employee.component';
 import { ManageUsersComponent } from './components/admin/manage-users/manage-users.component';
@@ -84,12 +83,6 @@ export const routes: Routes = [
     canActivate: [authGuard],
     data: { roles: ['Employee'] },
   },
-  // {
-  //   path: 'request-details/:id',
-  //   component: RequestDetailsComponent,
-  //   canActivate: [authGuard],
-  //   data: { roles: ['Employee'] },
-  // },
   {
     path: 'employee/services',
     component: ServicesComponent,
@@ -102,12 +95,6 @@ export const routes: Routes = [
     canActivate: [authGuard],
     data: { roles: ['Employee'] },
   },
-  // {
-  //   path: 'service-details/:id',
-  //   component: ServiceDetailsComponent,
-  //   canActivate: [authGuard],
-  //   data: { roles: ['Employee'] },
-  // },
 
   // ------------------------ Admin ------------------------
   {
