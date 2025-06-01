f88a701a798f1c93707ece70b5496cf3a52e29ea
Rula-Sh <1.rulashaban@gmail.com>
2025-06-01 20:27:18 +0300

    Improve Form Validation, Improve Phone Number and Job Title Feilds, and Add Job Category

--- src/app/models/Employee.model.ts
+++ src/app/models/Employee.model.ts
@@ -2,9 +2,31 @@ import { Service } from './Service.model';
 import { ServiceRequest } from './ServiceRequest.model';
 import { User } from './User.model';
 
+type JobCategory = 'Housekeeping' | 'Food & Beverage' | 'Guest Services' | 'Maintenance';
+
+export type JobTitle =
+  // Housekeeping
+  | 'Room Cleaner'
+  | 'Laundry Attendant'
+
+  // Dining
+  | 'Food & Beverage Server'
+
+  // Guest Services / Requests
+  | 'Valet Parking Attendant'
+  | 'Concierge'
+  | 'Porter'
+
+  // Maintenance
+  | 'Maintenance Technician'
+  | 'Electrician'
+  | 'Plumber'
+  | 'IT Support Technician';
+
 export interface Employee extends User {
   hotel: string;
-  jobTitle: string;
+  jobTitle: JobTitle;
+  jobCategory: JobCategory;
   services?: Service[];
   serviceRequests?: ServiceRequest[];
 }
