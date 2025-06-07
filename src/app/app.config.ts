1c643dac8b294d51b0af019ff10d8b65f422dcec
Rula-Sh <1.rulashaban@gmail.com>
2025-06-07 11:22:09 +0300

    Remove Unused Packages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { provideToastr } from 'ngx-toastr';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -11,13 +9,6 @@ import Aura from '@primeng/themes/aura';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideToastr({
-      positionClass: 'toast-top-center', // وسط أعلى الشاشة
-      timeOut: 3000,
-      closeButton: true,
-      progressBar: true,
-      preventDuplicates: true,
-    }),
     provideRouter(routes),
     provideHttpClient(),
     provideAnimationsAsync(),
